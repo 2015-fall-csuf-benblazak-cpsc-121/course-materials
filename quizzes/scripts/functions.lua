6fcc52c6452aaed9eca83d3f943273a1c0017a26
Ben Blazak <benblazak.dev@gmail.com>
2015-08-31 10:20:48 -0700

    quiz 2; using float for copyright line; split out setup

---
+++ quizzes/scripts/functions.lua
@@ -0,0 +1,10 @@
+-------------------------------------------------------------------------------
+-- Copyright &copy; 2015 Ben Blazak <bblazak@fullerton.edu>
+-- Released under the [MIT License] (http://opensource.org/licenses/MIT)
+-------------------------------------------------------------------------------
+
+function doc_title(s)
+  s = s:gsub( '(%a)(%a+)-(%d+).*', '%1%2 %3' ):gsub( '^%l', string.upper )
+  tex.print(s)
+end
+
