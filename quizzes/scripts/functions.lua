8005f5010707eea7b469ca58b3cd675f0c6fc775
Ben Blazak <benblazak.dev@gmail.com>
2015-08-31 19:08:00 -0700

    finished quizzes 01--03

--- quizzes/scripts/functions.lua
+++ quizzes/scripts/functions.lua
@@ -3,6 +3,11 @@
 -- Released under the [MIT License] (http://opensource.org/licenses/MIT)
 -------------------------------------------------------------------------------
 
+function doc_name(s)
+  s = s:gsub( '(%a+-%d+).*', '%1' )
+  tex.print(s)
+end
+
 function doc_title(s)
   s = s:gsub( '(%a)(%a+)-(%d+).*', '%1%2 %3' ):gsub( '^%l', string.upper )
   tex.print(s)
